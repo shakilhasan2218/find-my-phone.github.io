7a0a3f0cf084e8a83a5678aa26008f5ac80bbaa0
Md Shakil Hasan <82412240+shakilhasan2218@users.noreply.github.com>
2026-01-16 22:27:16 +0600

    Implement caching in service worker on install
    Add service worker install event to cache files

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("find-phone").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js"
+      ]);
+    })
+  );
+});
